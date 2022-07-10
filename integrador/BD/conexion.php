a257a8c65b33b27c5a5305c8ca0d9fe4202f396e
ruizosonozisrael33 <105523799+ruizosonozisrael33@users.noreply.github.com>
2022-07-10 17:20:37 -0500

    otro commit
    carpeta del proyecto

---
+++ integrador/BD/conexion.php
@@ -0,0 +1,16 @@
+<!-- Inicializamos un archivo de php -->
+<?php
+$conexion = new mysqli("localhost", "root", "", "mipaggames");
+
+if (!$conexion) {
+  echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+  echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+  echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+  exit;
+}
+
+echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
+echo "Información del host: " . mysqli_get_host_info($conexion) . PHP_EOL;
+
+
+?>
---
+++ integrador/asset/javascript/layaut.js
@@ -0,0 +1,5 @@
+/* para ejecutar jquery o javascript en html es con la etiqueta scrip */
+    $(document).ready(function(){
+      $('#menuContainer').load('/componentes/footer.html');
+      $('#footerContainer').load('/componentes/footer.html');
+    });
