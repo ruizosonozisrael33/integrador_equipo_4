55775d658eb5338aae0cb67ce750d4c1da0bcbe8
ruizosonozisrael33 <al222110736@gmail.com>
2022-07-10 18:11:35 -0500

    actualizacion de direccionamiento

--- integrador/asset/javascript/layaut.js
+++
@@ -1,5 +0,0 @@
-/* para ejecutar jquery o javascript en html es con la etiqueta scrip */
-    $(document).ready(function(){
-      $('#menuContainer').load('/componentes/footer.html');
-      $('#footerContainer').load('/componentes/footer.html');
-    });
