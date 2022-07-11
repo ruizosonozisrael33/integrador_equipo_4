ad8a6fc60bdf3ee80af07bbfb2cabff44c1dbd9f
ruizosonozisrael33 <al222110736@gmail.com>
2022-07-10 19:53:09 -0500

    actualizacion de navegacion

--- integrador/asset/javascript/hedfoot.js
+++ integrador/asset/javascript/hedfoot.js
@@ -1,5 +1,5 @@
 /* para ejecutar jquery o javascript en html es con la etiqueta scrip */
     $(document).ready(function(){
-      $('#menuContainer').load('/componentes/footer.html');
-      $('#footerContainer').load('/componentes/footer.html');
+      $('#menuContainer').load('/layout/heder.html');
+      $('#footerContainer').load('/layout/footer.html');
     });
